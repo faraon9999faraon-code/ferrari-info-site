b3a7b7941927638a3f07a7602485c106e902b2ea
Webmaster <faraon9999faraon@gmail.com>
2025-11-28 21:57:10 +0600

    Fix build.sh superuser auto-create

--- build.sh
+++ build.sh
@@ -2,5 +2,14 @@
 set -o errexit
 
 pip install -r requirements.txt
+
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
+
+# Создать суперюзера, только если не существует
+echo "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
+" | python manage.py shell
